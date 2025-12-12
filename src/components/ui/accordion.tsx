90ddec05ea57c8803322d51540e0dcef106f82fe
Suyash Kamath <suyashkamath9@gmail.com>
2025-12-12 23:35:37 +0530

    Fix CSS syntax errors: translate to transform, aspect-square to aspect-ratio

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -10,7 +10,7 @@ const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
 >(({ className, ...props }, ref) => (
-  <AccordionPrimitive.Item ref={ref} className={cn("border-b", className)} {...props} />
+  <AccordionPrimitive.Item ref={ref} className={cn("ui-accordion-item", className)} {...props} />
 ));
 AccordionItem.displayName = "AccordionItem";
 
@@ -21,10 +21,7 @@ const AccordionTrigger = React.forwardRef<
   <AccordionPrimitive.Header className="flex">
     <AccordionPrimitive.Trigger
       ref={ref}
-      className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
-        className,
-      )}
+      className={cn("ui-accordion-trigger", className)}
       {...props}
     >
       {children}
@@ -40,10 +37,10 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    className="ui-accordion-content"
     {...props}
   >
-    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    <div className={cn("ui-accordion-content-padding", className)}>{children}</div>
   </AccordionPrimitive.Content>
 ));
 
